2f08911418d50643c3e739f2757286bd0cbec984
akic404 <60869186+akic404@users.noreply.github.com>
2025-12-04 10:49:15 +0800

    优化指令表压入树的逻辑，增加内存分配错误检测，并为main层节点添加sub层节点支持

--- uart_at.c
+++ uart_at.c
@@ -44,67 +44,126 @@ void LD_cmdtreenode_init(LD_CmdTreeNode* current){//初始化单个树节点
     current->number_of_childs = 0 ;
 }
 
-void LD_cmd_put_in_tree(){//指令表压入树
+void LD_cmd_put_in_tree(){//指令表压入树,dps逻辑
 
     uint16_t cmd_array_node_size = sizeof(LD_cmd_array)/sizeof(LD_CmdArrayNode);//计算并存储有多少条命令
 
     uint8_t* used_main_index = calloc(cmd_array_node_size,sizeof(uint8_t));//建立此数组用于记录每条命令中的main部分是否已经被压入树
     if(used_main_index==NULL)LD_malloc_error_handler();//检测是否分配失败
 
-    uint16_t number_of_childs = 10;//记录分配的空间大小,注意每次处理到一个新节点的childs时都要重置到10
+    uint8_t* used_sub_index = calloc(cmd_array_node_size,sizeof(uint8_t));//建立此数组用于记录每条命令中的sub部分是否已经被压入树
+    if(used_sub_index==NULL)LD_malloc_error_handler();//检测是否分配失败
 
-    //>>>>> 添加main层节点start >>>>>
+    uint16_t space_of_root_node_childs = 10;//记录分配的空间大小
+
     for(int i = 0;i<cmd_array_node_size;i++){
-       //判断当前cmd的main是否已被压入树中
+        //>>>>> 添加main层节点start >>>>>
+
+        //判断当前cmd的main是否已被压入树中
         if(used_main_index[i]){
             //如果在'used_main_index'已经记录为已使用则跳过当前这条命令
             continue;
         }else{//如果未使用则进行压入
 
-            //获取记录当前main词
-            const char* current_main =LD_cmd_array[i].main;
+            //获取记录当前main使用的命令的指针
+            LD_CmdArrayNode* current_arraynode_for_main = &LD_cmd_array[i];
 
             //判断现有数据量是否到达阈值决定是否扩容根节点的childs空间
             //判断方式：若加上一条命令，已占用空间是否超过总空间的0.8
             //若超过：空间*2重新分配
-            if ((float)number_of_childs*0.8<root.number_of_childs+1){
-                number_of_childs *=2;
-                root.childs = realloc(root.childs,number_of_childs*sizeof(LD_CmdTreeNode*));
+            if (root.number_of_childs>(float)space_of_root_node_childs*0.8){
+                space_of_root_node_childs *=2;
+                root.childs = realloc(root.childs,space_of_root_node_childs*sizeof(LD_CmdTreeNode*));
                 if(root.childs==NULL)LD_malloc_error_handler();//检测是否分配失败
             }
 
             //添加节点到root.childs数组里
-            root.childs[root.number_of_childs] = calloc(1,sizeof(LD_CmdTreeNode));//开辟一个新的 树节点 空间 并返回其指针到root.childs这个 树节点指针数组 对应的位置
-            if(root.childs[root.number_of_childs]==NULL)LD_malloc_error_handler();//检测是否分配失败
+            LD_CmdTreeNode* current_treenode_level_main = calloc(1,sizeof(LD_CmdTreeNode));//开辟一个新的 树节点 空间
+            if(current_treenode_level_main==NULL)LD_malloc_error_handler();//检测是否分配失败
+            root.childs[root.number_of_childs]=current_treenode_level_main;//返回其指针到root.childs这个 树节点指针数组 对应的位置
 
-            //格式化一下这个新的节点并把对应的main词赋值到val里
-            LD_cmdtreenode_init(root.childs[root.number_of_childs]);
-            root.childs[root.number_of_childs]->val = current_main;
 
-            //更新“存储到root的子节点树”，此数字同时也作为索引使用，所有对新建子节点的操作需要在这行代码前执行
-            root.number_of_childs++;
+            //格式化一下这个新的节点并TODO:转移数据
+            LD_cmdtreenode_init(current_treenode_level_main);
+            current_treenode_level_main->val= current_arraynode_for_main->main;
 
             //给相同词语标记上已使用,加快入树速度
             for (int i2 = 0; i2 < cmd_array_node_size; i2++){
-                if(strcmp(current_main,LD_cmd_array[i2].main)==0){
+                if(strcmp(current_arraynode_for_main->main,LD_cmd_array[i2].main)==0){
                     used_main_index[i2] = 1;
                 }
             }
+
+            //>>>>> 为当前main层节点添加sub层节点start >>>>>
+            uint16_t space_of_main_node_childs = 10;//用于处理main节点用
+
+            for (int j = 0; j < cmd_array_node_size; j++) {
+                if(used_sub_index[j]==1){
+                    //如果在'used_sub_index'已经记录为已使用则跳过当前这条命令
+                    continue;
+                }else{//如果未使用则进行压入
+                    LD_CmdArrayNode* current_arraynode_for_sub =& LD_cmd_array[j];
+
+                    //判断当前遍历到的准备用来压入sub的命令 其main部分是不是等于上一步压入的main词,避免sub压入了错误的main下面
+                    if(strcmp(current_arraynode_for_main->main,current_arraynode_for_sub->main)!=0){
+                        continue;
+                    }else if(strcmp(current_arraynode_for_main->main,current_arraynode_for_sub->main)==0){
+
+                        //扩展空间,这里是和当前main节点的childs空间大小进行对比
+                        if (current_treenode_level_main->number_of_childs>(float)space_of_main_node_childs * 0.8 )
+                        {
+                            space_of_main_node_childs *=2;
+                            current_treenode_level_main->childs = realloc(current_treenode_level_main->childs,space_of_main_node_childs*sizeof(LD_CmdTreeNode*));
+                            if(current_treenode_level_main->childs==NULL)LD_malloc_error_handler();//检测是否分配失败
+                        }
+
+                        //新建一个treenode节点并存入当前main节点的childs对应位置
+                        LD_CmdTreeNode* current_treenode_level_sub = calloc(1,sizeof(LD_CmdTreeNode));
+                        if(current_treenode_level_sub==NULL)LD_malloc_error_handler();
+                        current_treenode_level_main->childs[current_treenode_level_main->number_of_childs]=current_treenode_level_sub;
+
+                        //初始化并TODO:转移数据
+                        LD_cmdtreenode_init(current_treenode_level_sub);
+                        current_treenode_level_sub->val = current_arraynode_for_sub->sub;
+
+                        current_treenode_level_main->number_of_childs++;
+
+                        //标记sub相同的命令为已使用
+                        for(int j2 = 0;j2<cmd_array_node_size;j2++){
+                            if(used_main_index[j2]==1){//这行用于避免标记了 未使用的 main 相同 sub的命令
+                                if(strcmp(current_arraynode_for_sub->sub,LD_cmd_array[j2].sub)==0){
+                                    used_sub_index[j2]=1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            //收回main节点的childs多余分配出去的空间
+            if(current_treenode_level_main->number_of_childs!=0){
+            current_treenode_level_main->childs=realloc(current_treenode_level_main->childs,current_treenode_level_main->number_of_childs*sizeof(LD_CmdTreeNode*));
+            }
+
+            //<<<<< 为当前main层节点添加sub层节点end <<<<<
+
+            //更新“存储到root的子节点数量”，此数字同时也作为索引使用，所有对新建main节点的操作需要在这行代码前执行,也包括在此main节点压入sub节点
+            root.number_of_childs++;
+
+
         }
-    }
-    //TODO:根据root节点记录的number_of_childs 用realloc缩小root.childs空间
+        //<<<<< 添加main层节点end <<<<<
 
-    //<<<<< 添加main层节点end <<<<<
-
-    //>>>>> TODO:为每个main层节点添加sub层节点start >>>>>
-    for(;;){
 
     }
-    //<<<<< 为每个main层节点添加sub层节点end <<<<<
 
-    //要不要释放原来的表？
+    //收回root节点childs多余分配出去的空间
+    if(root.number_of_childs!=0){
+        root.childs=realloc(root.childs,root.number_of_childs*sizeof(LD_CmdTreeNode*));
+    }
+
     //TODO:压入树功能未完成
 }
+//TODO：下面加个参数用于复制吧
 void LD_uart_idle_interrupt_process(){//中断空闲处理，每次接收到完整一条命令时执行
     memcpy(LD_uart_buffer_copy,LD_uart_buffer,LD_uart_buffer_size);
     LD_uart_cmd_process();
--- uart_at.h
+++ uart_at.h
@@ -36,12 +36,11 @@ typedef struct {//接受到的串口数据结构体
 
 
 
-
-
-
-
-
-
+void LD_init();//库初始化函数，开机时需要运行一次
+void LD_cmdtreenode_init(LD_CmdTreeNode* current);//初始化单个树节点
+void LD_cmd_put_in_tree();//指令表压入树,dps逻
+void LD_uart_idle_interrupt_process();//中断空闲处理，每次接收到完整一条命令时执行
+void LD_systick_1ms();//1ms定时服务，用于定时清理缓存区，现在没启用
 
 
 #if defined(__cplusplus)
